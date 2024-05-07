1a0ca6f8978bcef54e51c292a8361bc4f48b2e29
Simon Fikse <thearchseal@gmail.com>
2024-05-07 14:42:57 +0200

    first commit

---
+++ test.sql
@@ -0,0 +1,13 @@
+SELECT * FROM users;
+SELECT * FROM levels;
+SELECT * FROM ratings;
+SELECT * FROM comments;
+SELECT * FROM highscores;
+SELECT * FROM follows;
+
+DROP TABLE follows;
+DROP TABLE highscores;
+DROP TABLE comments;
+DROP TABLE ratings;
+DROP TABLE levels;
+DROP TABLE users;
